8d8ef34e04709c2af5858b4264bdb14d5fab6e2b
Jorge Rotter <jorgearotter@gmail.com>
2018-05-14 09:00:49 -0500

    Mejor impresión en klee-minty

--- tercer-parcial/klee-minty.py
+++ tercer-parcial/klee-minty.py
@@ -36,7 +36,15 @@ def genera_klee_minty(m):
 for m in [10,12,14,16]:
     A,b,c = genera_klee_minty(m)
     x0, (y0,z0), num_iter = newton.newton_pi(A, b, c, is_sparse=False)
+    x0 = x0[:-m]
+    c = c[:-m]
+    z0 = z0[:-m]
     print(f'Klee-Minty para m = {m}')
     print(f'El valor óptimo es {np.dot(c,x0)}')
     print(f'||diag(x)z||_\inf = {linalg.norm(x0*z0)}')
     print(f'Número de iteraciones: {num_iter}')
+    print(f'La solución óptima primal es: {x0}')
+    print('Que en la última coordenada es el valor óptimo, como debería.')
+    print(f'la solución del dual es {y0}')
+    print(f'Que está bien, puesto que, multiplicando tenemos {np.dot(b,y0)}')
+    print()
